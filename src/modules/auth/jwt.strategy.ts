030d9c60fa11b35bc27fe995f101571d5a3ba4a0
frazer <frazer.jin@gmail.com>
2023-11-11 12:24:49 +0800

    default token

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -6,7 +6,7 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(protected readonly configService: ConfigService) {
-    const jwtSecret = configService.get('JWT_TOKEN'); // 获取JWT令牌并存储在变量中
+    const jwtSecret = configService.get('JWT_TOKEN') || 'your-secret-key'; // 获取JWT令牌并存储在变量中
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: true,
